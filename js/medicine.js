1d105ecca06b5c7c3d32d5fc618bc254b76806bb
jeremyfelt <jeremy.felt@gmail.com>
2014-08-22 13:45:00 -0700

    Add handling of featured image backgrounds with Skrollr
    Works on secondary pages right now

--- js/medicine.js
+++ js/medicine.js
@@ -4,11 +4,21 @@
 
 	$(document).ready(function(){
 		var featured_image = $('.featured-image');
+		var margin_section = $('.skrollr-margin-section');
+		var h1_header = $('.h1-header');
 
-		featured_image.attr( 'data-0', 'transform: translate3d(0px,0px,0px);' );
-		featured_image.attr( 'data-' + 275, 'transform: translate3d(0px, 70px, 0px);');
+		// The featured image area should start at 350px and shrink in height to 0 while the
+		// background image appears that it remains in the same place.
+		featured_image.attr( 'data-0', 'height: 350px; background-position-y:0;position: fixed; top:0;' );
+		featured_image.attr( 'data-' + 350, 'height: 0; background-position-y:0;position: fixed; top: 0;');
 
-		medicine_skrollr = skrollr.init({forceHeight: false, smoothScrolling: true});
+		// The header for the page should track the moving section as it goes up the page
+		// and over the featured image.
+		h1_header.attr( 'data-' + 0, 'position: fixed; top: 349px;' );
+		h1_header.attr( 'data-' + 350, 'position: fixed; top: 0px;' );
+
+		// Fire skrollr
+		medicine_skrollr = skrollr.init({forceHeight: true, smoothScrolling: true});
 
 	});
 
