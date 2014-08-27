8545112d646a763b0f24d132560c9f999eafc2ea
jeremyfelt <jeremy.felt@gmail.com>
2014-08-27 09:32:26 -0700

    Add a mobile check to the Skrollr init and disable if true
    The effect we're using for the featured images does not translate
    well to the mobile view right now. It should be disabled.

--- js/medicine.js
+++ js/medicine.js
@@ -48,33 +48,45 @@
 
 	}
 
+	/**
+	 * Determine if the page view is occurring on a mobile device, as the skrollr functionality
+	 * cannot be relied on at that point.
+	 *
+	 * @returns {*|boolean}
+	 */
+	function is_mobile() {
+		return (/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera);
+	}
+
 	$(document).ready(function(){
 		var featured_image = $('.featured-image');
 		var h1_header = $('.h1-header');
 		var home_page_pic = $('.pic1');
 		var home_page_pic_title = $('.pic1 .center');
 
-		// The featured image area should start at 350px and shrink in height to 0 while the
-		// background image appears that it remains in the same place.
-		featured_image.attr( 'data-0', 'height: 350px; background-position-y:0;position: fixed; top:0;' );
-		featured_image.attr( 'data-' + 350, 'height: 0; background-position-y:0;position: fixed; top: 0;');
+		if ( false === is_mobile() ) {
+			// The featured image area should start at 350px and shrink in height to 0 while the
+			// background image appears that it remains in the same place.
+			featured_image.attr( 'data-0', 'height: 350px; background-position-y:0;position: fixed; top:0;' );
+			featured_image.attr( 'data-' + 350, 'height: 0; background-position-y:0;position: fixed; top: 0;');
 
-		// The header for the page should track the moving section as it goes up the page
-		// and over the featured image.
-		h1_header.attr( 'data-' + 0, 'position: fixed; top: 349px;' );
-		h1_header.attr( 'data-' + 350, 'position: fixed; top: 0px;' );
+			// The header for the page should track the moving section as it goes up the page
+			// and over the featured image.
+			h1_header.attr( 'data-' + 0, 'position: fixed; top: 349px;' );
+			h1_header.attr( 'data-' + 350, 'position: fixed; top: 0px;' );
 
-		// The home page image should start at 700px high and shrink in height to 0 while the
-		// background image appears that it remains in the same place.
-		home_page_pic.attr( 'data-0', 'height:700px; width: 1188px;background-position-y:0;position:fixed; top:0;');
-		home_page_pic.attr( 'data-700', 'height:0px; width:1188px;background-position-y:0;position:fixed; top:0;');
+			// The home page image should start at 700px high and shrink in height to 0 while the
+			// background image appears that it remains in the same place.
+			home_page_pic.attr( 'data-0', 'height:700px; width: 1188px;background-position-y:0;position:fixed; top:0;');
+			home_page_pic.attr( 'data-700', 'height:0px; width:1188px;background-position-y:0;position:fixed; top:0;');
 
-		// The title inside the home page image should move to the top slightly slower than everything else.
-		home_page_pic_title.attr( 'data-0', 'margin-top: 270px;' );
-		home_page_pic_title.attr( 'data-700', 'margin-top: -100px;' );
+			// The title inside the home page image should move to the top slightly slower than everything else.
+			home_page_pic_title.attr( 'data-0', 'margin-top: 270px;' );
+			home_page_pic_title.attr( 'data-700', 'margin-top: -100px;' );
 
-		// Fire skrollr
-		medicine_skrollr = skrollr.init({forceHeight: true, smoothScrolling: true});
+			// Fire skrollr
+			medicine_skrollr = skrollr.init({forceHeight: true, smoothScrolling: true});
+		}
 
 		setup_graph_on_scroll();
 
