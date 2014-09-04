d3c5853cd3d30db1d5a48f44522ae84f67d551e7
jeremyfelt <jeremy.felt@gmail.com>
2014-09-04 16:02:09 -0700

    Store the page ID to load next on a specific page

--- includes/load-page-next.php
+++ includes/load-page-next.php
@@ -6,6 +6,70 @@ class WSU_Med_Load_Page_Next {
 	 */
 	public function __construct() {
 		add_action( 'get_footer', array( $this, 'add_next_main_section' ) );
+		add_action( 'init', array( $this, 'setup' ) );
+		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+	}
+
+	/**
+	 * Begin the meta box adding process.
+	 */
+	public function setup() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ), 10, 2 );
+	}
+
+	/**
+	 * Add meta boxes used for Load Page Next functionality.
+	 *
+	 * @param string $post_type Current post type being edited.
+	 */
+	public function add_meta_boxes( $post_type ) {
+		if ( 'page' === $post_type ) {
+			add_meta_box( 'wsu_med_load_page_next', 'Page to load next', array( $this, 'load_page_next_meta' ), $post_type, 'side' );
+		}
+	}
+
+	/**
+	 * Show an input field for the page ID associated with a post.
+	 */
+	public function load_page_next_meta() {
+		$page_id = get_post_meta( get_the_ID(), '_med_page_load_next_id', true );
+
+		if ( 0 === absint( $page_id ) ) {
+			$page_id = '';
+		}
+
+		?><label for="wsu_med_page_id_next">Page ID</label>
+		<input type="text" name="wsu_med_page_id_next" id="wsu_med_page_id_next" value="<?php echo esc_attr( $page_id ); ?>" /><?php
+	}
+
+	/**
+	 * Save the page ID associated with the next page load.
+	 *
+	 * @param int     $post_id Current post ID.
+	 * @param WP_Post $post    Current post object.
+	 */
+	public function save_post( $post_id, $post ) {
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			return;
+		}
+
+		if ( false !== wp_is_post_revision( $post_id ) ) {
+			return;
+		}
+
+		if ( 'page' === $post->post_type && isset( $_POST['wsu_med_page_id_next'] ) && current_user_can( 'edit_post', $post_id ) ) {
+			$page_id = absint( $_POST['wsu_med_page_id_next'] );
+
+			if ( $page_id === 0 ) {
+				return;
+			}
+
+			update_post_meta( $post_id, '_med_page_load_next_id', $page_id );
+		}
 	}
 
 	/**
