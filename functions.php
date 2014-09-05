06dab67dd99c4f7647348fdd56d78c3c61832482
jeremyfelt <jeremy.felt@gmail.com>
2014-09-05 14:43:54 -0700

    Bump version to 0.10.1

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ include_once( __DIR__ . '/includes/load-page-next.php' );
  * @return string Current script version
  */
 function wsu_medicine_script_version() {
-	return spine_get_script_version() . '0.10.0';
+	return spine_get_script_version() . '0.10.1';
 }
 
 add_action( 'wp_footer', 'wsu_medicine_sharing_footer' );
